421f1b6e47ef9de180e94947414c677a8ba0c1aa
leander <leander.apel@gmail.com>
2024-05-02 11:13:45 +0200

    301/501

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'DartPilot',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.black),
         useMaterial3: true,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
@@ -32,6 +32,14 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
+  int _playerCount = 2; // Default player count
+  int _selectedGameMode = 301; // Default game mode
+  bool isSingle = true;
+  bool is301 = true;
+  int selectedPlayers = 1;
+
+  List<String> _playerNames =
+      List.generate(4, (index) => 'Spieler ${index + 1}');
 
   void _incrementCounter() {
     setState(() {
@@ -42,30 +50,182 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+            Text(
+              'Anzahl von Spieler:',
+              style: TextStyle(fontSize: 20.0),
+            ),
+            SizedBox(height: 20.0),
+            Wrap(
+              alignment: WrapAlignment.center,
+              children: List.generate(4, (index) {
+                int playerCount = index + 1;
+                return Padding(
+                  padding: EdgeInsets.all(5.0),
+                  child: ElevatedButton(
+                    onPressed: () {
+                      setState(() {
+                        selectedPlayers = playerCount;
+                        _playerNames = List.generate(
+                            selectedPlayers, (index) => 'Spieler ${index + 1}');
+                        print("$selectedPlayers");
+                      });
+                    },
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: selectedPlayers == playerCount
+                          ? Colors.blue[100]
+                          : Colors.grey[300],
+                      padding: EdgeInsets.all(15.0),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10.0),
+                        side: BorderSide(
+                          color: playerCount == 5
+                              ? Colors.black
+                              : Colors.transparent,
+                          width: 2.0,
+                        ),
+                      ),
+                    ),
+                    child: Text(
+                      '$playerCount',
+                      style: TextStyle(fontSize: 20.0),
+                    ),
+                  ),
+                );
+              }),
+            ),
+            SizedBox(height: 20),
+            Text(
+              'Spielernamen:',
+              textAlign: TextAlign.left,
+            ),
+            SizedBox(height: 10),
+            Column(
+              children: _playerNames.map((name) {
+                return TextFormField(
+                  initialValue: name,
+                  onChanged: (value) {
+                    _playerNames[_playerNames.indexOf(name)] = value;
+                  },
+                  decoration: InputDecoration(
+                    labelText: 'Spieler ${_playerNames.indexOf(name) + 1}',
+                  ),
+                );
+              }).toList(),
             ),
             Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+              'Spielmodus:',
+              textAlign: TextAlign.left,
             ),
-            FilledButton(
+            SizedBox(height: 20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                SizedBox(height: 10),
+                ElevatedButton(
+                  onPressed: () {
+                    setState(() {
+                      is301 = true;
+                    });
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor:
+                        is301 ? Colors.blue[100] : Colors.grey[300],
+                  ),
+                  child: Text(
+                    '301',
+                    style: TextStyle(
+                      color: Colors.black, // Hier änderst du die Farbe
+                      fontSize: 30.0,
+                    ),
+                  ),
+                ),
+                SizedBox(width: 20),
+                ElevatedButton(
+                  onPressed: () {
+                    setState(() {
+                      is301 = false;
+                    });
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor:
+                        !is301 ? Colors.blue[100] : Colors.grey[300],
+                  ),
+                  child: Text(
+                    '501',
+                    style: TextStyle(
+                      color: Colors.black, // Hier änderst du die Farbe
+                      fontSize: 30.0,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            Text(
+              'Out:',
+              textAlign: TextAlign.left,
+            ),
+            SizedBox(height: 20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                SizedBox(height: 20),
+                ElevatedButton(
+                  onPressed: () {
+                    setState(() {
+                      isSingle = true;
+                    });
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor:
+                        isSingle ? Colors.blue[100] : Colors.grey[300],
+                  ),
+                  child: Text(
+                    'Single',
+                    style: TextStyle(
+                      color: Colors.black, // Hier änderst du die Farbe
+                      fontSize: 20.0,
+                    ),
+                  ),
+                ),
+                SizedBox(width: 20),
+                ElevatedButton(
+                  onPressed: () {
+                    setState(() {
+                      isSingle = false;
+                    });
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor:
+                        !isSingle ? Colors.blue[100] : Colors.grey[300],
+                  ),
+                  child: Text(
+                    'Double',
+                    style: TextStyle(
+                      color: Colors.black, // Hier änderst du die Farbe
+                      fontSize: 20.0,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: 20),
+            Center(
+              child: ElevatedButton(
                 onPressed: () {
                   Navigator.push(
                     context,
                     MaterialPageRoute(builder: (context) => GameScreen()),
                   );
                 },
-                child: const Text('Filled')
-            )
+                child: Text('Start'),
+              ),
+            ),
           ],
         ),
       ),
